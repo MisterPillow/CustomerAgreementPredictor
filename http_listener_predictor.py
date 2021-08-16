f259428d5b1d0365f8a801458bcd1900ecf55a8a
Dmitry Bolshakov <32982665+MisterPillow@users.noreply.github.com>
2021-08-16 15:05:29 +0300

    Make logging code more pretty

--- http_listener_predictor.py
+++ http_listener_predictor.py
@@ -1,6 +1,7 @@
 import re
 from pymorphy2 import MorphAnalyzer
 import nltk
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.base import BaseEstimator
@@ -19,22 +20,8 @@ class Lemmatizer(BaseEstimator):
     patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
     morph = MorphAnalyzer()
 
-    def fit(self, X, y):
-        if isinstance(X, np.ndarray):
-            # Check that X and y have correct shape
-            X, y = check_X_y(X, y)
-        self.X_ = X
-        self.y_ = y
-        return self
-
     def transform(self, X):
-        if isinstance(X, np.ndarray):
-            X = check_array(X, accept_sparse=True)
-            return X
         return X.apply(self.lemmatize)
-
-    #     def fit_transform(self, X, y):
-    #         return self.fit(X, y).transform(X)
 
     def lemmatize(self, line):
         data = re.sub(self.patterns, ' ', line)
@@ -60,18 +47,23 @@ def lem_features(data, features):
 
 model = pickle.load(open(filename, 'rb'))
 app = Flask(__name__)
-features = ['CustomerInitMessage', 'SellerAnswer', 'CustomerFollowingMessage']
+text_features = ['CustomerInitMessage', 'SellerAnswer', 'CustomerFollowingMessage']
+
+
+def format_param(param):
+    return "\"{0}\"".format(param) if isinstance(param, str) else "{0}".format(param)
 
 
 @app.route('/baro', methods=['POST'])
 def baro_post_request():
     try:
         # Debug output
-        print("Request received. PrevBaro: ", request.json['PrevBaro'], ",",
-              "CustomerInitMessage: ", "\"{0}\",".format(request.json['CustomerInitMessage']),
-              "SellerAnswer: ", "\"{0}\",".format(request.json['SellerAnswer']),
-              "CustomerFollowingMessage: ", "\"{0}\",".format(request.json['CustomerFollowingMessage']),
-              flush=True)  # Required to print message to log when app is started via gunicorn
+        print("Request received. PrevBaro: {0}, CustomerInitMessage: {1}, SellerAnswer: {2}, CustomerFollowingMessage: {3}".format(
+            request.json['PrevBaro'],
+            format_param(request.json['CustomerInitMessage']),
+            format_param(request.json['SellerAnswer']),
+            format_param(request.json['CustomerFollowingMessage'])
+        ), flush=True) # Required to print message to log when app is started via gunicorn
         data = {
             'PrevBaro': request.json['PrevBaro'],
             'CustomerInitMessage': request.json['CustomerInitMessage'],
@@ -79,7 +71,7 @@ def baro_post_request():
             'CustomerFollowingMessage': request.json['CustomerFollowingMessage']
         }
         x = pd.DataFrame(data, index=[0])
-        lem_features(x, features)
+        lem_features(data=x, features=text_features)
         y = model.predict(x)
         return jsonify({'result': y[0]})
     except Exception as ex:
